74874268c8e587b77a0be5b1e0e2494c1c089811
Nick Borrello <nvborrello@gmail.com>
2025-04-23 23:30:11 -0400

    gitignore

--- logic/term_loader.py
+++ logic/term_loader.py
@@ -1,6 +1,14 @@
 import json
+import os
+import sys
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and PyInstaller """
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 def load_terms(json_path):
-    with open(json_path, "r") as f:
+    full_path = resource_path(json_path)
+    with open(full_path, "r") as f:
         return json.load(f)
-
